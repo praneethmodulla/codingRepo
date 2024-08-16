43a4bbecf23ba9570f661c484abf86f6421f5199
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2024-08-16 15:10:30 +0000

    Sync LeetCode submission - Valid Anagram (java)

---
+++ my-folder/problems/valid_anagram/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for(int i = 0; i < t.length(); i++){
+            map1.put(t.charAt(i), map1.getOrDefault(t.charAt(i), 0) - 1);
+        }
+        for(Character c : map1.keySet()){
+            if(map1.get(c) != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
