c8126b1a38479eb0b0ff40a363acd1d79d933d3e
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2024-10-08 19:30:56 +0000

    Sync LeetCode submission - House Robber (java)

--- my-folder/problems/house_robber/solution.java
+++ my-folder/problems/house_robber/solution.java
@@ -1,22 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1){
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        int maxVal = recurse(nums.length - 1, nums, dp);
+        return maxVal;
+    }
+
+    public int recurse(int ind, int[] nums, int[] dp){
+        if(ind == 0){
             return nums[0];
         }
-        if(nums.length == 0){
+        if(ind < 0){
             return 0;
         }
-        int maxVal = Integer.MIN_VALUE;
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        maxVal = Math.max(dp[0], dp[1]);
-        for(int i = 2; i < nums.length; i++){
-            for(int k = i - 2; k >= 0; k--){
-                dp[i] = Math.max(dp[i], nums[i] + dp[k]);
-            }
-            maxVal = Math.max(maxVal, dp[i]);
+        if(dp[ind] != -1){
+            return dp[ind];
         }
-        return maxVal; 
+        int pick = nums[ind] + recurse(ind - 2, nums, dp);
+        int nonPick = 0 + recurse(ind - 1, nums, dp);
+
+        return dp[ind] = Math.max(pick, nonPick);
     }
 }
