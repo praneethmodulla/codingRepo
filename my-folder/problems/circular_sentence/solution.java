5545a2a2ac912da9d623585c00cf9df00e83a6f7
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2024-11-02 09:06:42 +0000

    Sync LeetCode submission - Circular Sentence (java)

---
+++ my-folder/problems/circular_sentence/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        String[] str = sentence.split(" ");
+        for(int i = 0; i < str.length - 1; i++){
+            if(str[i].charAt(str[i].length() - 1) != str[i + 1].charAt(0)){
+                return false;
+            }
+        }
+        if(str[0].charAt(0) != str[str.length - 1].charAt(str[str.length - 1].length() - 1)){
+            return false;
+        }
+        return true;
+    }
+}
