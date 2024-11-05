b832d34035aafca112db1af7dd4ef1caed197227
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2024-11-05 21:01:31 +0000

    Sync LeetCode submission - Minimum Number of Changes to Make Binary String Beautiful (java)

---
+++ my-folder/problems/minimum_number_of_changes_to_make_binary_string_beautiful/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minChanges(String s) {
+        if(s.length() % 2 == 1){
+            return 0;
+        }
+        int cnt = 0;
+        for(int i = 0; i < s.length() - 1; i += 2){
+            if(s.charAt(i) != s.charAt(i + 1)){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
