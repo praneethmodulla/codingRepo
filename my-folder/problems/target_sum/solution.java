b9207faa731ef2261d7f0b4c5e1c897947844123
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2024-12-26 00:32:39 +0000

    Sync LeetCode submission - Target Sum (java)

--- my-folder/problems/target_sum/solution.java
+++ my-folder/problems/target_sum/solution.java
@@ -1,23 +1,23 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return recurse(nums, target, nums.length - 1);
+        return recurse(nums, nums.length - 1, target);
     }
 
-    public int recurse(int[] nums, int target, int idx){
+    public int recurse(int[] nums, int idx, int target){
         if(idx == 0){
-            if(nums[idx] == 0 && target == 0){
-                return 2; 
+            if(target == 0 && nums[idx] == 0){
+                return 2;
             }
             if(target - nums[idx] == 0){
                 return 1;
-            } else if(target + nums[idx] == 0){
-                return 1;
-            } else {
-                return 0;
             }
+            if(target + nums[idx] == 0){
+                return 1;
+            }
+            return 0;
         }
-        int add = recurse(nums, target - nums[idx], idx - 1);
-        int subtract = recurse(nums, target + nums[idx], idx - 1);
+        int add = recurse(nums, idx - 1, target - nums[idx]);
+        int subtract = recurse(nums, idx - 1, target + nums[idx]);
         return add + subtract;
     }
 }
