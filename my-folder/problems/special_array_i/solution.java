5d75995046e0d04d0e2b833636f6128732dbcb58
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-02-06 13:55:12 +0000

    Sync LeetCode submission - Special Array I (java)

---
+++ my-folder/problems/special_array_i/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i - 1] % 2 == 0){
+                if(nums[i] % 2 == 0){
+                    return false;
+                }
+            } else {
+                if(nums[i] % 2 == 1){
+                    return false;
+                } 
+            }
+        }
+        return true;
+    }
+}
