734ccc9415e22ed5e5342f0a4248a9f98491a315
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-02-09 20:07:35 +0000

    Sync LeetCode submission - Move Zeroes (java)

--- my-folder/problems/move_zeroes/solution.java
+++ my-folder/problems/move_zeroes/solution.java
@@ -1,18 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums.length == 0 || nums.length == 1){
-            return;
-        }
         int i = 0;
-        for(int j = 0; j < nums.length; j++){
-            if(nums[i] != 0){
-                i++;
-            } else {
-                if(i != j && nums[j] != 0){
-                    nums[i] = nums[j];
-                    nums[j] = 0;
-                    i++;
-                }
+        int j = 0;
+        for(i = 0; i < nums.length; i++){
+            if(i == j && nums[i] != 0){
+                j++;
+            } else if(i != j && nums[i] != 0){
+                nums[j] = nums[i];
+                nums[i] = 0;
+                j++;
             }
         }
         return;
