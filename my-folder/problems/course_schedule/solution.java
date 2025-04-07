046bdb0e9845c5952c344698bbb8942580dd9d85
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-04-07 16:19:53 +0000

    Sync LeetCode submission - Course Schedule (java)

--- my-folder/problems/course_schedule/solution.java
+++ my-folder/problems/course_schedule/solution.java
@@ -1,31 +1,23 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adjList = new ArrayList<List<Integer>>();
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<List<Integer>>();
         for(int i = 0; i < numCourses; i++){
-            adjList.add(new ArrayList<Integer>());
+            adj.add(new ArrayList<Integer>());
         }
         for(int i = 0; i < prerequisites.length; i++){
-            adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
-        int[] indegree = new int[numCourses];
-        for(int i = 0; i < adjList.size(); i++){
-            List<Integer> nodes = adjList.get(i);
-            for(int j = 0; j < nodes.size(); j++){
-                indegree[nodes.get(j)]++;
-            }
-        } 
         Queue<Integer> queue = new LinkedList<Integer>();
         for(int i = 0; i < indegree.length; i++){
             if(indegree[i] == 0){
                 queue.add(i);
             }
         }
-        if(queue.isEmpty()){
-            return false;
-        }
         while(!queue.isEmpty()){
             int node = queue.poll();
-            List<Integer> nodes = adjList.get(node);
+            List<Integer> nodes = adj.get(node);
             for(int i = 0; i < nodes.size(); i++){
                 indegree[nodes.get(i)]--;
                 if(indegree[nodes.get(i)] == 0){
@@ -34,7 +26,7 @@ class Solution {
             }
         }
         for(int i = 0; i < indegree.length; i++){
-            if(indegree[i] != 0){
+            if(indegree[i] > 0){
                 return false;
             }
         }
