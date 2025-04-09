5d959ad968a94a9981ae26df9822d4d2b3199f80
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-04-09 15:25:32 +0000

    Sync LeetCode submission - Lemonade Change (java)

--- my-folder/problems/lemonade_change/solution.java
+++ my-folder/problems/lemonade_change/solution.java
@@ -1,28 +1,39 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        if(bills[0] != 5){
-            return false;
-        }
-        int five = 1;
-        int ten = 0;
-        for(int i = 1; i < bills.length; i++){
-            if(bills[i] == 5){
-                five++;;
-            } else if(bills[i] == 10){
-                if(five > 0){
-                    five--;
-                    ten++;
-                } else {
-                    return false;
-                }
-            } else if(bills[i] == 20){
-                if(five > 0 && ten > 0){
-                    five--;
-                    ten--;
-                } else if(five >= 3){
-                    five -= 3;
-                } else {
-                    return false;
+        HashMap<Integer, Integer> hMap = new HashMap<Integer, Integer>();
+        for(int i = 0; i < bills.length; i++){
+            int change = bills[i] - 5;
+            hMap.put(bills[i], hMap.getOrDefault(bills[i], 0) + 1);
+            if(change > 0){
+                if(change == 5){
+                    if(!hMap.containsKey(5)){
+                        return false;
+                    } else if(hMap.get(5) >= 1){
+                        int num = hMap.get(5);
+                        num--;
+                        hMap.put(5, num);
+                    } else {
+                        return false;
+                    }
+                } else if(change == 15){
+                    if(hMap.containsKey(10) || hMap.containsKey(5)){
+                        if(hMap.containsKey(10) && hMap.get(10) >= 1 && hMap.containsKey(5) && hMap.get(5) >= 1){
+                            int num1 = hMap.get(10);
+                            int num2 = hMap.get(5);
+                            num1--;
+                            num2--;
+                            hMap.put(10, num1);
+                            hMap.put(5, num2);
+                        } else if (hMap.containsKey(5) && hMap.get(5) >= 3){
+                            int num = hMap.get(5);
+                            num = num - 3;
+                            hMap.put(5, num); 
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
                 }
             }
         }
