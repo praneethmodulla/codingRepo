ef799266d9cc09fd3c28f50624fcfb33443d6c4f
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-04-09 18:34:50 +0000

    Sync LeetCode submission - Jump Game (java)

--- my-folder/problems/jump_game/solution.java
+++ my-folder/problems/jump_game/solution.java
@@ -1,33 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length == 1){
-            return true;
-        }
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        recurse(nums, 0, dp);
-        if(dp[0] == 1){
-            return true;
-        }
-        return false;
-    }
-
-    public int recurse(int[] nums, int idx, int[] dp){
-        if(idx == nums.length - 1){
-            return 1;
-        }
-
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        for(int i = 1; i <= nums[idx]; i++){
-            int num = 0;
-            num = recurse(nums, idx + i, dp);
-            if(num == 1){
-                return dp[idx] = num;
+        int max = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i > max){
+                return false;
             }
+            max = Math.max(max, nums[i] + i);
         }
-        
-        return dp[idx] = 0;
+        return true;
     }
 }
