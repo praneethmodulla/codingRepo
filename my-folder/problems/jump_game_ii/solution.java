a103daede7bd72c48db0a379b75caffd6b511465
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-04-12 22:45:54 +0000

    Sync LeetCode submission - Jump Game II (java)

--- my-folder/problems/jump_game_ii/solution.java
+++ my-folder/problems/jump_game_ii/solution.java
@@ -1,26 +1,17 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return recurse(nums, 0, dp);
-    }
-
-    public int recurse(int[] nums, int idx, int[] dp){
-        if(idx >= nums.length){
-            return (int)Math.pow(10,9);
-        }
-        if(idx == nums.length - 1){
-            return 0;
-        }
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        int numJumps = (int)Math.pow(10, 9);
-        for(int i = 1; i <= nums[idx]; i++){
-            if(idx + i < nums.length){
-                numJumps = Math.min(numJumps, 1 + recurse(nums, idx + i, dp));
+        int jumps = 0; 
+        int l = 0;
+        int r = 0;
+        int farthest = 0;
+        while(r < nums.length - 1){
+            for(int i = l; i <= r; i++){
+                farthest = Math.max(nums[i] + i, farthest);
             }
+            l = r + 1;
+            r = farthest;
+            jumps = jumps + 1;
         }
-        return dp[idx] = numJumps;
+        return jumps;
     }
 }
