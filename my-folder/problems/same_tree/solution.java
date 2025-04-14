a92625cac79ea99d7860d45e0969d3d1fc88589c
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-04-14 19:02:36 +0000

    Sync LeetCode submission - Same Tree (java)

--- my-folder/problems/same_tree/solution.java
+++ my-folder/problems/same_tree/solution.java
@@ -17,14 +17,20 @@ class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null){
             return true;
+        } else if((p == null && q != null) || (p != null && q == null)){
+            return false;
         }
-        if(p == null || q == null){
+        boolean left = isSameTree(p.left, q.left);
+        if(!left){
+            return false;
+        }
+        boolean right = isSameTree(p.right, q.right);
+        if(!right){
             return false;
         }
         if(p.val != q.val){
-            return false; 
-        } else {
-            return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+            return false;
         }
+        return left && right;
     }
 }
