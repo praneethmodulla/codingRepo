19bac26f7981392c3b260fd99726cb78c164c505
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-04-23 03:35:25 +0000

    Sync LeetCode submission - Palindrome Partitioning (java)

--- my-folder/problems/palindrome_partitioning/solution.java
+++ my-folder/problems/palindrome_partitioning/solution.java
@@ -1,34 +1,31 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<List<String>>();
-        List<String> path = new ArrayList<String>();
-        res = recurse(res, path, s, 0);
-        return res;
+        List<List<String>> result = new ArrayList<List<String>>();
+        List<String> res = new ArrayList<String>();
+        recurse(s, result, res, 0);
+        return result;
     }
 
-    public List<List<String>> recurse(List<List<String>> res, List<String> path, String s, int idx){
+    public void recurse(String s, List<List<String>> result, List<String> res, int idx){
         if(idx == s.length()){
-            res.add(new ArrayList<String>(path));
-            return res;
+            result.add(new ArrayList<String>(res));
+            return;
         }
-        for(int i = idx; i < s.length(); i++){
-            if(checkPalindrome(s.substring(idx, i + 1))){
-                path.add(s.substring(idx, i + 1));
-                res = recurse(res, path, s, i + 1);
-                path.remove(path.size() - 1);
+        for(int i = idx + 1; i <= s.length(); i++){
+            if(checkPalindrome(s.substring(idx, i))){
+                res.add(s.substring(idx, i));
+                recurse(s, result, res, i);
+                res.remove(res.size() - 1);
             }
         }
-        return res;
+        return;
     }
 
-    public boolean checkPalindrome(String str){
-        if(str.length() == 1){
-            return true;
-        }
+    public boolean checkPalindrome(String s){
         int i = 0;
-        int j = str.length() - 1;
+        int j = s.length() - 1;
         while(i <= j){
-            if(str.charAt(i) != str.charAt(j)){
+            if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
             i++;
@@ -36,5 +33,4 @@ class Solution {
         }
         return true;
     }
-
 }
