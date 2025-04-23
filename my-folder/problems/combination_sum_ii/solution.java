7f88fbf2fa6ab963cae5f93399fd8383716fcd1a
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-04-23 02:34:55 +0000

    Sync LeetCode submission - Combination Sum II (java)

--- my-folder/problems/combination_sum_ii/solution.java
+++ my-folder/problems/combination_sum_ii/solution.java
@@ -2,38 +2,30 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<Integer> set = new ArrayList<Integer>();
-        int sum = 0;
-        result = recurse(result, set, candidates, target, sum, 0);
+        List<Integer> res = new ArrayList<Integer>();
+        recurse(candidates, target, 0, result, res);
         return result;
     }
 
-    public List<List<Integer>> recurse(List<List<Integer>> result, List<Integer> set, int[] candidates, int target, int sum, int idx){
-        if(idx == candidates.length){
-            if(sum == target){
-                List<Integer> copySet = new ArrayList<Integer>(set);
-                result.add(copySet);
+    public void recurse(int[] nums, int target, int idx, List<List<Integer>> result, List<Integer> res){
+        if(idx == nums.length){
+            if(target == 0){
+                result.add(new ArrayList<Integer>(res));
             }
-            return result;
+            return;
         }
-        if(sum > target){
-            return result;
+        if(target < 0){
+            return;
         }
-        if(sum == target){
-            List<Integer> copySet = new ArrayList<Integer>(set);
-            result.add(copySet);
-            return result;
+        res.add(nums[idx]);
+        recurse(nums, target - nums[idx], idx + 1, result, res);
+        res.remove(res.size() - 1);
+        int j = idx;
+        while(j < nums.length && nums[j] == nums[idx]){
+            j++;
         }
-        for(int i = idx; i < candidates.length; i++){
-            if(i > idx && candidates[i] == candidates[i - 1]){
-                continue;
-            }
-            sum += candidates[i];
-            set.add(candidates[i]);
-            result = recurse(result, set, candidates, target, sum, i + 1);
-            sum -= candidates[i];
-            set.remove(set.size() - 1);
-        }
-        return result;
+        idx = j;
+        recurse(nums, target, idx, result, res);
+        return;
     }
 }
