6d79714561cac6cba570aa595ecdec8620a06026
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-04-23 02:53:15 +0000

    Sync LeetCode submission - Subsets II (java)

--- my-folder/problems/subsets_ii/solution.java
+++ my-folder/problems/subsets_ii/solution.java
@@ -1,22 +1,26 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<Integer> set = new ArrayList<Integer>();
-        result = recurse(result, set, nums, 0);
+        List<Integer> res = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        recurse(nums, 0, result, res);
         return result;
     }
 
-    public List<List<Integer>> recurse(List<List<Integer>> result, List<Integer> set, int[] nums, int idx){
-        result.add(new ArrayList<Integer>(set));
-        for(int i = idx; i < nums.length; i++){
-            if(i != idx && nums[i] == nums[i - 1]){
-                continue;
-            }
-            set.add(nums[i]);
-            result = recurse(result, set, nums, i + 1);
-            set.remove(set.size() - 1);
+    public void recurse(int[] nums, int idx, List<List<Integer>> result, List<Integer> res){
+        if(idx == nums.length){
+            result.add(new ArrayList<Integer>(res));
+            return;
         }
-        return result;
+        res.add(nums[idx]);
+        recurse(nums, idx + 1, result, res);
+        res.remove(res.size() - 1);
+        int j = idx;
+        while(j < nums.length && nums[j] == nums[idx]){
+            j++;
+        }
+        idx = j;
+        recurse(nums, idx, result, res);
+        return;
     }
 }
