cf39c0488e81274fa554365e55797bddee85325a
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-04-23 03:43:54 +0000

    Sync LeetCode submission - Word Search (java)

--- my-folder/problems/word_search/solution.java
+++ my-folder/problems/word_search/solution.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean checkExists = false;
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == word.charAt(0)){
-                    checkExists = recurse(board, word, i, j, 0);
-                    if(checkExists){
-                        return checkExists;
+                    if(recurse(board, i, j, word, 0)){
+                        return true;
                     }
                 }
             }
@@ -14,31 +12,23 @@ class Solution {
         return false;
     }
 
-    public boolean recurse(char[][] board, String word, int row, int col, int idx){
+    public boolean recurse(char[][] board, int row, int col, String word, int idx){
         if(idx == word.length()){
             return true;
         }
-        if(row < 0 || row >= board.length){
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length){
             return false;
         }
-        if(col < 0 || col >= board[0].length){
-            return false;
-        }
-        if(board[row][col] == '1'){
-            return false;
-        }
-
         if(board[row][col] == word.charAt(idx)){
             char temp = board[row][col];
-            board[row][col] = '1';
-            boolean checkUp = recurse(board, word, row - 1, col, idx + 1);
-            boolean checkDown = recurse(board, word, row + 1, col, idx + 1);
-            boolean checkLeft = recurse(board, word, row, col - 1, idx + 1);
-            boolean checkRight = recurse(board, word, row, col + 1, idx + 1);
+            board[row][col] = '0';
+            boolean up = recurse(board, row - 1, col, word, idx + 1);
+            boolean down = recurse(board, row + 1, col, word, idx + 1);
+            boolean right = recurse(board, row, col + 1, word, idx + 1);
+            boolean left = recurse(board, row, col - 1, word, idx + 1);
             board[row][col] = temp;
-            return checkUp || checkDown || checkLeft || checkRight;
+            return up || down || left || right; 
         }
-
         return false;
     }
 }
