ebfbe9692eb7a092980f1ea895089a035d8a4df2
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-05-01 14:59:36 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (java)

--- my-folder/problems/find_first_and_last_position_of_element_in_sorted_array/solution.java
+++ my-folder/problems/find_first_and_last_position_of_element_in_sorted_array/solution.java
@@ -1,39 +1,45 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[2];
-        res[0] = -1;
-        res[1] = -1;
-
         int low = 0;
         int high = nums.length - 1;
-        int mid = 0;
         while(low <= high){
-            mid = (low + high)/2;
-            if(nums[mid] < target){
-                low = mid + 1;
-            } else {
-                if(mid < nums.length && nums[mid] == target){
-                    res[0] = mid;
-                }
+            int mid = (low + high)/2;
+            if(nums[mid] == target){
+                return range(mid, nums);
+            } else if(nums[mid] > target){
                 high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int[] range(int mid, int[] nums){
+        int[] res = new int[]{-1, -1};
+        int i = mid;
+        int j = mid;
+        while(i >= 0){
+            if(nums[i] == nums[mid]){
+                i--;
+            } else {
+                res[0] = i + 1;
+                break;
             }
         }
         if(res[0] == -1){
-            return res;
+            res[0] = i + 1;
         }
-        low = 0;
-        high = nums.length - 1;
-        mid = 0;
-        while(low <= high){
-            mid = (low + high)/2;
-            if(nums[mid] <= target){
-                if(mid < nums.length && nums[mid] == target){
-                    res[1] = mid;
-                }
-                low = mid + 1;
+        while(j < nums.length){
+            if(nums[j] == nums[mid]){
+                j++;;
             } else {
-                high = mid - 1;
+                res[1] = j - 1;
+                break;
             }
+        }
+        if(res[1] == -1){
+            res[1] = j - 1;
         }
         return res;
     }
