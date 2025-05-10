2104b55aa363a822e655326b8ab21f6c8863ae7f
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-05-10 20:48:21 +0000

    Sync LeetCode submission - Binary Search (java)

--- my-folder/problems/binary_search/solution.java
+++ my-folder/problems/binary_search/solution.java
@@ -1,13 +1,18 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return recurse(nums, target, 0, nums.length - 1);
-    }
-    public int recurse(int[] nums, int target, int low, int high){
-        if(low > high) return -1;
-
-        int mid = (low + high)/2;
-        if(nums[mid] == target) return mid;
-        else if(nums[mid] > target) return recurse(nums, target, low, mid - 1);
-        return recurse(nums, target, mid + 1, high);
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] > target){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
     }
 }
