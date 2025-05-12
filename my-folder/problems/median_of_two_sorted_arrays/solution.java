84fdeb3096f17a145464544112fb3a7c5a857944
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-05-12 09:56:28 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (java)

---
+++ my-folder/problems/median_of_two_sorted_arrays/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        if(len1 > len2){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int low = 0;
+        int high = len1;
+        int left = (len1 + len2 + 1)/2;
+        while(low <= high){
+            int mid1 = (low + high)/2;
+            int mid2 = left - mid1;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            if(mid1 < len1) r1 = nums1[mid1];
+            if(mid2 < len2) r2 = nums2[mid2];
+            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            if(l1 <= r2 && l2 <= r1){
+                if((len1 + len2)%2 == 1){
+                    return (double)Math.max(l1, l2);
+                } else {
+                    return ((double)Math.max(l1, l2) + (double)Math.min(r1, r2)) / 2.0;
+                }
+            } else if(l1 > r2){
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
+            }
+        }
+        return 0;
+    }
+}
