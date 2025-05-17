be77ef6f3478ae9d886711f5db92055ed97f522b
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-05-17 00:26:00 +0000

    Sync LeetCode submission - Sort Colors (java)

--- my-folder/problems/sort_colors/solution.java
+++ my-folder/problems/sort_colors/solution.java
@@ -1,13 +1,18 @@
 class Solution {
     public void sortColors(int[] nums) {
+        HashMap<Integer, Integer> hMap = new HashMap<Integer, Integer>();
         for(int i = 0; i < nums.length; i++){
-            for(int j = 0; j < nums.length - 1 - i; j++){
-                if(nums[j] > nums[j + 1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
+            hMap.put(nums[i], hMap.getOrDefault(nums[i], 0) + 1);
+        }
+        int j = 0;
+        for(int i = 0; i < 3; i++){
+            int k = hMap.getOrDefault(i, 0);
+            while(k > 0){
+                nums[j] = i;
+                j++;
+                k--;
             }
         }
+        return;
     }
 }
