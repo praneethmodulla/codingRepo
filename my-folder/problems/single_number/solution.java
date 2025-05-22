c8a8a5c95429ecb0a002e1dbd5519efe4ee4d28e
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-05-22 09:39:39 +0000

    Sync LeetCode submission - Single Number (java)

--- my-folder/problems/single_number/solution.java
+++ my-folder/problems/single_number/solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
+        int val = 0;
         for(int i = 0; i < nums.length; i++){
-            ans ^= nums[i];
+            val = val ^ nums[i];
         }
-        return ans;
+        return val;
     }
 }
