c4250f495e4b20009b35e663442843a703d3dd5f
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-05-22 10:06:05 +0000

    Sync LeetCode submission - Shortest Word Distance (java)

---
+++ my-folder/problems/shortest_word_distance/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int idx1 = -1;
+        int idx2 = -1;
+        int minDiff = Integer.MAX_VALUE;;
+        for(int i = 0; i < wordsDict.length; i++){
+            if(wordsDict[i].equals(word1)){
+                idx1 = i;
+            }
+            if(wordsDict[i].equals(word2)){
+                idx2 = i;
+            }
+            if(idx1 != -1 && idx2 != -1){
+                if(minDiff > Math.abs(idx1 - idx2)){
+                    minDiff = Math.abs(idx1 - idx2);
+                }
+            }
+        }
+        return minDiff;
+    }
+}
