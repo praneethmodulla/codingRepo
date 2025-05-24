df84f2552ad267285fdc8f67ed4c66db01cc421f
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-05-24 19:52:06 +0000

    Sync LeetCode submission - Combination Sum (java)

--- my-folder/problems/combination_sum/solution.java
+++ my-folder/problems/combination_sum/solution.java
@@ -1,25 +1,31 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<Integer> res = new ArrayList<Integer>();
-        recurse(candidates, target, res, result, 0);
-        return result;
+       List<List<Integer>> result = new ArrayList<List<Integer>>();
+       List<Integer> set = new ArrayList<Integer>();
+       recurse(candidates, target, 0, 0, set, result);
+       return result;
     }
 
-    public void recurse(int[] nums, int target, List<Integer> res, List<List<Integer>> result, int idx){
-        if(target < 0){
+    public void recurse(int[] candidates, int target, int sum, int idx, List<Integer> set, List<List<Integer>> res){
+        if(sum > target){
             return;
         }
-        if(idx == nums.length){
-            if(target == 0){
-                result.add(new ArrayList<Integer>(res));
-            }
+        if(sum == target){
+            List<Integer> arr = new ArrayList<Integer>(set);
+            res.add(arr);
             return;
         }
-        res.add(nums[idx]);
-        recurse(nums, target - nums[idx], res, result, idx);
-        res.remove(res.size() - 1);
-        recurse(nums, target, res, result, idx + 1);
+        if(idx == candidates.length){
+            return;
+        }
+        recurse(candidates, target, sum, idx + 1, set, res);
+        sum += candidates[idx];
+        set.add(candidates[idx]);
+        recurse(candidates, target, sum, idx, set, res);
+        sum -= candidates[idx];
+        set.remove(set.size() - 1);
         return;
     }
+
+    
 }
