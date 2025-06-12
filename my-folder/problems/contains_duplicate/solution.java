f44d431fea0703e3cd04cd993a8d87a872f281be
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-06-12 18:36:07 +0000

    Sync LeetCode submission - Contains Duplicate (java)

--- my-folder/problems/contains_duplicate/solution.java
+++ my-folder/problems/contains_duplicate/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<Integer>();
         for(int i = 0; i < nums.length; i++){
             if(!set.contains(nums[i])){
                 set.add(nums[i]);
