59bc4385a939db76e6b01f8a345e6a2f3120d404
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-10-02 06:57:23 +0000

    Sync LeetCode submission - Group Anagrams (java)

--- my-folder/problems/group_anagrams/solution.java
+++ my-folder/problems/group_anagrams/solution.java
@@ -1,21 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
+        List<List<String>> result = new ArrayList<List<String>>();
+        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+        int[] count = new int[26];
         for(int i = 0; i < strs.length; i++){
-            char[] strArray = strs[i].toCharArray();
-            Arrays.sort(strArray);
-            String str = new String(strArray);
-            List<String> s = new ArrayList<>();
-            if(map.containsKey(str)){
-                s = map.get(str);
-                s.add(strs[i]);
-                map.put(str, s);
+            Arrays.fill(count, 0);
+            for(int j = 0; j < strs[i].length(); j++){
+                count[strs[i].charAt(j) - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int k = 0; k < 26; k++){
+                sb.append("#");
+                sb.append(count[k]);
+            }
+            if(map.containsKey(sb.toString())){
+                map.get(sb.toString()).add(strs[i]);
             } else {
-                s.add(strs[i]);
-                map.put(str, s);
+                map.put(sb.toString(), new ArrayList<String>());
+                map.get(sb.toString()).add(strs[i]);
             }
         }
-        List<List<String>> result = new ArrayList<List<String>>();
         for(String s : map.keySet()){
             result.add(map.get(s));
         }
