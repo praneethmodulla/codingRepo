ba284d38162d7d0334401ccf8967081369dc5b3f
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-10-03 14:07:31 +0000

    Sync LeetCode submission - Valid Palindrome (java)

--- my-folder/problems/valid_palindrome/solution.java
+++ my-folder/problems/valid_palindrome/solution.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
+            if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'){
+                sb.append((char)(s.charAt(i) + 32));
+            } else if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
                 sb.append(s.charAt(i));
             } else if(s.charAt(i) >= '0' && s.charAt(i) <= '9'){
                 sb.append(s.charAt(i));
             }
         }
-        String finalString = sb.toString();
-        System.out.println(finalString);
-        for(int i = 0; i < finalString.length()/2; i++){
-            if(finalString.charAt(i) != finalString.charAt(finalString.length() - 1 - i)){
+        String str = sb.toString();
+        int i = 0;
+        int j = str.length() - 1;
+
+        while(i <= j){
+            if(str.charAt(i) == str.charAt(j)){
+                i++;
+                j--;
+            } else {
                 return false;
             }
         }
