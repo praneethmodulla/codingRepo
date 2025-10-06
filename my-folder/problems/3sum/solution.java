3f93f42c23452ca68c40445cdf15ec4269a89aad
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-10-06 15:00:06 +0000

    Sync LeetCode submission - 3Sum (java)

--- my-folder/problems/3sum/solution.java
+++ my-folder/problems/3sum/solution.java
@@ -1,35 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
         for(int i = 0; i < nums.length; i++){
-            if(i != 0 && nums[i] == nums[i - 1]){
-                continue;
-            }
-            int j = i + 1;
-            int k = nums.length - 1;
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum > 0){
-                    k--;
-                } else if(sum < 0){
-                    j++;
-                } else {
-                    List<Integer> res = new ArrayList<>();
-                    res.add(nums[i]);
-                    res.add(nums[j]);
-                    res.add(nums[k]);
-                    result.add(res);
-                    k--;
-                    j++;
-                    while(j < k && nums[j] == nums[j - 1]){
+            if(i == 0 || nums[i] != nums[i - 1]){
+                int target = 0 - nums[i];
+                int j = i + 1;
+                int k = nums.length - 1;
+                while(j < k){
+                    if(nums[j] + nums[k] == target){
+                        List<Integer> res = new ArrayList<Integer>();
+                        res.add(nums[i]);
+                        res.add(nums[j]);
+                        res.add(nums[k]);
+                        result.add(res);
+                        j++;
+                        k--;
+                        while(j < nums.length && nums[j - 1] == nums[j]){
+                            j++;
+                        }
+                        while(k >= 0 && nums[k + 1] == nums[k]){
+                            k--;
+                        }
+                    } else if(nums[j] + nums[k] > target){
+                        k--;
+                    } else if(nums[j] + nums[k] < target){
                         j++;
                     }
-                    while(j < k && nums[k] == nums[k + 1]){
-                        k--;
-                    }
                 }
-                
             }
         }
         return result;
