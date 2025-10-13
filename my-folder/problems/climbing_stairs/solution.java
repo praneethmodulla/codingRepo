2aa61a15ae062c2bfea57a653a729b94767c3f4b
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-10-13 04:00:14 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- my-folder/problems/climbing_stairs/solution.java
+++ my-folder/problems/climbing_stairs/solution.java
@@ -1,9 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n < 2){
-            return 1;
+        if(n <= 2){
+            return n;
         }
         int[] dp = new int[n + 1];
+        dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
         for(int i = 3; i <= n; i++){
