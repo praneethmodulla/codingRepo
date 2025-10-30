dc169e23a0051b3ac98b11b5bffa99d33caca8fd
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-10-30 14:51:10 +0000

    Sync LeetCode submission - Counting Bits (java)

---
+++ my-folder/problems/counting_bits/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for(int i = 0; i <= n; i++){
+            ans[i] = setBits(i);
+        }
+        return ans;
+    }
+
+    public int setBits(int n){
+        int count = 0;
+        while(n != 0){
+            if((n & 1) == 1){
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+}
