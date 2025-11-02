c8cfb3343afdec8d1ff878b8637a920b5a254a26
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-11-02 21:04:46 +0000

    Sync LeetCode submission - Hamming Distance (java)

--- my-folder/problems/hamming_distance/solution.java
+++ my-folder/problems/hamming_distance/solution.java
@@ -1,10 +1,12 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int xorResult = x ^ y;
+        int a = x ^ y;
         int count = 0;
-        while(xorResult != 0){
-            count += xorResult & 1;
-            xorResult >>>= 1;
+        while(a != 0){
+            if((a & 1) == 1){
+                count++;
+            }
+            a = a >> 1;
         }
         return count;
     }
