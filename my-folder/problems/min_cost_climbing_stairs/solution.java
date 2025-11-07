05ee9e5159fcdb6725d4103fca95685cbabad2c9
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-11-07 12:55:19 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (java)

--- my-folder/problems/min_cost_climbing_stairs/solution.java
+++ my-folder/problems/min_cost_climbing_stairs/solution.java
@@ -2,10 +2,11 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int[] dp = new int[n + 1];
+
         dp[n] = 0;
-        dp[n - 1] = cost[n - 1]; 
+        dp[n - 1] = cost[n - 1];
         for(int i = n - 2; i >= 0; i--){
-            dp[i] = cost[i] + Math.min(dp[i + 1], dp[i + 2]);
+            dp[i] = cost[i] + Math.min(dp[i + 2], dp[i + 1]);
         }
         return Math.min(dp[0], dp[1]);
     }
