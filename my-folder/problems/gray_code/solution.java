ca1baafabb3b85178a73af6c2a5c02a43007a66a
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-11-08 23:38:17 +0000

    Sync LeetCode submission - Gray Code (java)

--- my-folder/problems/gray_code/solution.java
+++ my-folder/problems/gray_code/solution.java
@@ -1,13 +1,10 @@
 class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> res = new ArrayList<Integer>();
-        res.add(0);
-        for(int i = 1; i <= n; i++){
-            int prev = res.size();
-            int mask = 1 << (i - 1);
-            for(int j = prev - 1; j >= 0; j--){
-                res.add(mask + res.get(j));
-            }
+        int len = 1 << n;
+        for(int i = 0; i < len; i++){
+            int num = i ^ (i >> 1);
+            res.add(num);
         }
         return res;
     }
