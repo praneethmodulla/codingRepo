07405453a6a02787731d4cb68330a019346c83d2
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-11-08 14:42:07 +0000

    Sync LeetCode submission - Count Subarrays With Majority Element I (java)

---
+++ my-folder/problems/count_subarrays_with_majority_element_i/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            int freq = 0;
+            for(int j = i; j < nums.length; j++){
+                if(nums[j] == target){
+                    freq++;
+                }
+                if((j - i + 1)/2 < freq){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
