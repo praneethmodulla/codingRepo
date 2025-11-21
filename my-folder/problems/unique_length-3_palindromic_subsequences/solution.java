082a13f7a2222e2d5ef575fec4604279b389018c
Medha Praneeth Reddy Modulla <141873665+praneethmodulla@users.noreply.github.com>
2025-11-21 13:05:26 +0000

    Sync LeetCode submission - Unique Length-3 Palindromic Subsequences (java)

--- my-folder/problems/unique_length-3_palindromic_subsequences/solution.java
+++ my-folder/problems/unique_length-3_palindromic_subsequences/solution.java
@@ -9,17 +9,16 @@ class Solution {
             int i = -1;
             int j = 0;
             for(int k = 0; k < s.length(); k++){
-                if(s.charAt(k) == ch){
+                if(ch == s.charAt(k)){
                     if(i == -1){
                         i = k;
                     }
                     j = k;
                 }
             }
-
             HashSet<Character> between = new HashSet<Character>();
-            for(int m = i + 1; m < j; m++){
-                between.add(s.charAt(m));
+            for(int g = i + 1; g < j; g++){
+                between.add(s.charAt(g));
             }
             ans += between.size();
         }
